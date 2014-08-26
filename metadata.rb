8fa93277f1eec21ff12e268fe6d9f4830719b579
Martin Fenner <mfenner@plos.org>
2014-08-26 14:39:11 +0200

    added metadata, use path to current folder

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,15 @@
 name              "rails"
-
+maintainer        "Martin Fenner"
+maintainer_email  "mfenner@plos.org"
+license           "Apache 2.0"
+description       "Configure Rails applications"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.1.0"
 depends           "apt", "2.3.8"
 depends           "git", "4.0.0"
 depends           "build-essential", "2.0.0"
 depends           "database", "~> 2.3.0"
+
+%w{ ubuntu }.each do |platform|
+  supports platform
+end
