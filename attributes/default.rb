099726c8bdcdb61376a9e15aead239346b26061f
Martin Fenner <mfenner@plos.org>
2014-09-03 01:19:05 +0200

    don't track nginx default server

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,5 +9,4 @@ default['ruby']['gems'] = %w{ bundler }
 default['ruby']['user'] = 'vagrant'
 default['ruby']['group'] = 'www-data'
 default['ruby']['rails_env'] = "development"
-default['ruby']['web'] = { 'default_server' => true }
 default['ruby']['db'] = { 'username' => 'vagrant', 'password' => SecureRandom.hex(10), 'host' => 'localhost' }
