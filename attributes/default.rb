eee67e42fed95c041750a67978ad0578503221f2
Martin Fenner <mfenner@plos.org>
2014-09-02 21:55:34 +0200

    include default attributes for rails applications

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,13 @@
+require 'securerandom'
+
 default['ruby']['version'] = "2.1"
 default['ruby']['install_dev_package'] = true
 default['ruby']['packages'] = %w{ libxml2-dev libxslt-dev libmysqlclient-dev nodejs }
 default['ruby']['gems'] = %w{ bundler }
+
+# default attributes for Rails applications
+default['ruby']['user'] = 'vagrant'
+default['ruby']['group'] = 'www-data'
+default['ruby']['rails_env'] = "development"
+default['ruby']['web'] = { 'default_server' => true }
+default['ruby']['db'] = { 'user' => 'vagrant', 'password' => SecureRandom.hex(10), 'host' => 'localhost' }
