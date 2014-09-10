2a8fadaf76ff4ac3a196f211f138aedb8642b5df
Martin Fenner <mfenner@plos.org>
2014-09-10 13:18:00 +0200

    set default group to vagrant

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,11 +2,14 @@ require 'securerandom'
 
 default['ruby']['version'] = "2.1"
 default['ruby']['install_dev_package'] = true
-default['ruby']['packages'] = %w{ libxml2-dev libxslt-dev libmysqlclient-dev nodejs }
+default['ruby']['packages'] = %w{ curl git libxml2-dev libxslt-dev libmysqlclient-dev nodejs }
 default['ruby']['gems'] = %w{ bundler }
 
 # default attributes for Rails applications
 default['ruby']['user'] = 'vagrant'
-default['ruby']['group'] = 'www-data'
+default['ruby']['group'] = 'vagrant'
 default['ruby']['rails_env'] = "development"
-default['ruby']['db'] = { 'username' => 'vagrant', 'password' => SecureRandom.hex(10), 'host' => 'localhost' }
+default['ruby']['db'] = {
+  'username' => 'vagrant',
+  'password' => SecureRandom.hex(10),
+  'host' => 'localhost' }
