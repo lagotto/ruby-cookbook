2a63aefd41fbe19653d20da561689de0a029edd3
Martin Fenner <mfenner@plos.org>
2014-10-20 11:22:35 +0200

    use ENV variables

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,15 +1,4 @@
-require 'securerandom'
-
 default['ruby']['version'] = "2.1"
 default['ruby']['install_dev_package'] = true
 default['ruby']['packages'] = %w{ curl git libmysqlclient-dev nodejs }
 default['ruby']['gems'] = %w{ bundler }
-
-# default attributes for Rails applications
-default['ruby']['user'] = 'vagrant'
-default['ruby']['group'] = 'vagrant'
-default['ruby']['rails_env'] = "development"
-default['ruby']['db'] = {
-  'username' => 'vagrant',
-  'password' => SecureRandom.hex(10),
-  'host' => 'localhost' }
