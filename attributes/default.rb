2356dc05ef1537f74b17f2c8d3d9e542a757e60f
Martin Fenner <mfenner@plos.org>
2015-03-09 08:43:49 +0100

    install ruby 2.2

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['ruby']['version'] = "2.1"
+default['ruby']['version'] = "2.2"
 default['ruby']['install_dev_package'] = true
 default['ruby']['packages'] = %w{ curl git libmysqlclient-dev python-software-properties software-properties-common }
 default['ruby']['gems'] = %w{ bundler }
