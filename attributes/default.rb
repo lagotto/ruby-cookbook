f1e35ca71b97fb465c9738504c731165ae281e01
Martin Fenner <mfenner@plos.org>
2015-08-06 19:56:00 +0200

    install package required for passenger

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default['ruby']['version'] = "2.2"
 default['ruby']['install_dev_package'] = true
-default['ruby']['packages'] = %w(git libmysqlclient-dev python-software-properties software-properties-common)
+default['ruby']['packages'] = %w(git libmysqlclient-dev python-software-properties software-properties-common apt-transport-https)
 default['ruby']['gems'] = %w(bundler)
