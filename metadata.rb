b928ee409c4c8a1281cf1d31edbaebbabfa99e4c
Martin Fenner <mfenner@plos.org>
2015-08-09 08:55:53 +0200

    updated metadata

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,12 @@
 name              "ruby"
 maintainer        "Martin Fenner"
-maintainer_email  "mfenner@plos.org"
+maintainer_email  "martin.fenner@datacite.org"
 license           "Apache 2.0"
 description       "Installs Ruby using the Brightbox Ruby PPA"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.7.3"
+version           "0.7.4"
+
+# opscode cookbooks
 depends           "apt"
 
 %w{ ubuntu }.each do |platform|
