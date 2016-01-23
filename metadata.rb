d40a6dc987080615a0c126101ec041efcaa78dc8
Martin Fenner <martin.fenner@datacite.org>
2016-01-23 11:40:13 +0100

    use ruby 2.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'martin.fenner@datacite.org'
 license           'Apache 2.0'
 description       'Installs Ruby using the Brightbox Ruby PPA'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '0.7.6'
+version           '0.7.7'
 
 # opscode cookbooks
 depends           'apt'
